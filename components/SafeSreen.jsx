754abafb6a1b29ac3034950de40311f8dc17ba26
withinnaija <withinnaija@gmail.com>
2026-07-07 23:20:10 +0100

    Initial commit

---
+++ components/SafeSreen.jsx
@@ -0,0 +1,20 @@
+import { View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { COLORS } from "../constants/colors";
+
+const SafeSreen = ({ children }) => {
+  const insect = useSafeAreaInsets();
+  return (
+    <View
+      style={{
+        // paddingTop: insect.top,
+        flex: 1,
+        backgroungColor: COLORS.primary,
+      }}
+    >
+      {children}
+    </View>
+  );
+};
+
+export default SafeSreen;
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,16 +1,9 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import React from 'react';
-import { useColorScheme } from 'react-native';
-
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
+import { Stack } from "expo-router";
+import SafeScreen from "../../components/SafeSreen";
+export default function RootLayout() {
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
+    <SafeScreen>
+      <Stack screenOptions={{ headerShown: false }} />
+    </SafeScreen>
   );
 }
